56f73dda6b36091df99853c6901b9864d088b01a
Derek Stobbs <dstobbs@googlemail.com>
2019-10-29 10:16:59 +0000

    Number Wizard Console
    - New project, with basic intro text

---
+++ 2_Number Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		Debug.Log("Welcome to Number Wizard");
+		Debug.Log("Pick a number, don't tell me what it is ...");
+		Debug.Log("Lowest number is: 1");
+		Debug.Log("Highest number is: 1000");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
